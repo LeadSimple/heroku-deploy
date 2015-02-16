68159c0f67e950f1369cdb7b10b1b12da52dd49b
Daniel Berkompas <himself@danielberkompas.com>
2015-02-16 07:47:49 -0800

    Use app name when backing up
    This is necessary when you have multiple remotes in your repo and they
    aren't named "heroku".

--- lib/heroku/deploy/tasks/database_backup.rb
+++ lib/heroku/deploy/tasks/database_backup.rb
@@ -11,7 +11,7 @@ module Heroku::Deploy::Task
       env_vars['RAILS_ENV'] = 'production'
 
       task "Backing up the remote database" do
-        shell "heroku pgbackups:capture --expire"
+        shell "heroku pgbackups:capture --expire --app #{app.name}"
       end
     end
   end
